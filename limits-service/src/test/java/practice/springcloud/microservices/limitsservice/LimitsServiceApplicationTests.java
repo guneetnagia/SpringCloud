cec96c9578840cea081d4a46483e673f6d1a36ba
Guneet Singh <guneetnagia24@gmail.com>
2020-01-10 14:52:06 +0530

    microservice--limits-service

---
+++ limits-service/src/test/java/practice/springcloud/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.springcloud.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
